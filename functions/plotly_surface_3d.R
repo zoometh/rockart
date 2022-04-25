09008d4e16b20f34844e9e14a41810f3ff61179c
zoometh <thomashuet7@gmail.com>
2022-04-25 21:40:23 +0100

    export surf3d

--- functions/plotly_surface_3d.R
+++ functions/plotly_surface_3d.R
@@ -2,24 +2,41 @@ library(plotly)
 
 url.www <- "https://raw.githubusercontent.com/zoometh/rockart/main/www/"
 xyz <- read.csv(paste0(url.www, "/confoc_3d.xyz"), sep = " ", header = F)
-xyz.df <- do.call(rbind, lapply(split(xyz, xyz$V1),
-                                function(d) `names<-`(xyz$V3, xyz$V2)))[,3:1]
-colors <- colorRamp(c("blue", "lightblue", "chartreuse3", "yellow", "red"))
+
 axx <- list(
-  title = "x"
+  title = "X \u03BCm"
 )
 axy <- list(
-  title = "y"
+  title = "Y \u03BCm"
 )
 axz <- list(
-  title = "z"
+  title = "Z \u03BCm"
 )
-plot_ly(x = ~xyz$V1, y = ~xyz$V2, z = ~xyz$V3,
+
+## surface
+surf3d <- plot_ly(x = ~xyz$V1, y = ~xyz$V2, z = ~xyz$V3,
         intensity = ~xyz$V3, type = 'mesh3d',
-        colors = colors,
-        colorbar = list(title = "z"))  %>%
+        colorbar = list(title = "Z \u03BCm"))  %>%
   layout(title = "ConFocal surface",
          scene = list(xaxis = axx, yaxis = axy, zaxis = axz,
                       aspectmode = 'data'))
+surf3d
 
+## surface with a marker
+surf3d.mark <- plot_ly()%>%
+  add_trace(type = "scatter3d", mode="markers", 
+            x = 1282.68, y = 1246.93, z = -9.86,
+            marker = list(size = 10,
+                          color = '#FF0000',
+                          opacity = .3)
+            ) %>%
+  add_trace(x = ~xyz$V1, y = ~xyz$V2, z = ~xyz$V3,
+            intensity = ~xyz$V3, type = 'mesh3d',
+            colorbar = list(title = "Z \u03BCm")) %>%
+  layout(title = "ConFocal surface",
+         scene = list(xaxis = axx, yaxis = axy, zaxis = axz,
+                      aspectmode = 'data'))
+surf3d.mark
 
+htmlwidgets::saveWidget(as_widget(surf3d), "surf3d.html")
+htmlwidgets::saveWidget(as_widget(surf3d), "surf3d.mark.html")
