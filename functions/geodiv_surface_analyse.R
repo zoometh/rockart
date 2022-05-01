4dff7257a68aa9e353605bb72906ce3f7c4eff46
zoometh <thomashuet7@gmail.com>
2022-05-01 16:25:46 +0100

    simplified

--- functions/geodiv_surface_analyse.R
+++ functions/geodiv_surface_analyse.R
@@ -13,3 +13,8 @@ df.var <- data.frame(abbrev = c('Sq', 'Sa', 'Sku', 'Ssk'),
                      descr = c('Root mean square height', 'Average roughness', 'Kurtosis', 'Skewness'),
                      val = c(sq.var, sa.var, sku.var, ssk.var))
 df.var
+# abbrev                   descr         val
+# 1     Sq Root mean square height 774.3146686
+# 2     Sa       Average roughness 677.9315813
+# 3    Sku                Kurtosis  -0.8874419
+# 4    Ssk                Skewness   0.6288069
