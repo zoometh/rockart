dea5601221f534a62fd9a22bce3d012cf0838e78
zoometh <thomashuet7@gmail.com>
2022-05-01 10:21:17 +0100

    simplified

--- functions/geodiv_surface_analyse.R
+++ functions/geodiv_surface_analyse.R
@@ -1,7 +1,7 @@
 library(geodiv)
-library(knitr)
-library(kableExtra)
+library(dplyr)
 
+# xyz <- read.table("confoc_3d.xyz", header = FALSE, sep = "")
 xyz.mat <- as.matrix(xyz)
 
 sq.var <- sq(xyz.mat)
@@ -9,8 +9,7 @@ sa.var <- sa(xyz.mat)
 sku.var <- sku(xyz.mat)
 ssk.var <- ssk(xyz.mat)
 
-df.var <- data.frame(abbrev = c('Sq', 'Sa', 'Sku', 'Ssk', '...'),
-                     descr = c('Root mean square height', 'Average roughness', 'Kurtosis', 'Skewness', '...'),
-                     val = c(sq.var, sa.var, sku.var, ssk.var, '...'))
-kable(df.var,"html") %>% 
-  kable_styling(full_width = FALSE, position = "center", font_size = 12)
+df.var <- data.frame(abbrev = c('Sq', 'Sa', 'Sku', 'Ssk'),
+                     descr = c('Root mean square height', 'Average roughness', 'Kurtosis', 'Skewness'),
+                     val = c(sq.var, sa.var, sku.var, ssk.var))
+df.var
